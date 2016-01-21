c320616b8202d21772f13d5f524f9eb8264195fb
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-01-21 03:45:25 -0500

    added delete for geometry objects
    I will go through the code for sunpath tomorrow to make sure I delete
    all the object inside sunpath too but so far at least I can run multiple
    sunlight hours analysis with no problem except it takes very long.

--- plugin/source/genskymtx.py
+++ plugin/source/genskymtx.py
@@ -1,12 +1,12 @@
 # add IronPython path to sys
 import sys
+import copy
 IronPythonLib = 'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(IronPythonLib)
 
 # Now that the path to IronPython is stablished we can import libraries
 import os
 import clr
-import copy
 clr.AddReference('DynamoCore')
 
 def getPackagePath(packageName):
@@ -19,17 +19,16 @@ def getPackagePath(packageName):
 sys.path.append(getPackagePath('Ladybug'))
 
 ###### start you code from here ###
-import ladybugdynamo.sunlighthours as sunlighthours
+import ladybugdynamo.sky as sky
 
 
 # get input data
-sunVectors = IN[0]
-testPoints = IN[1]
-geometries = IN[2]
+epwfile = IN[0]
+skyDensity = IN[1]
+workingDir = IN[2]
 
-slh = sunlighthours.Sunlighthours(sunVectors, [], testPoints, geometries)
-slh.runAnalysis()
+cSky = sky.CumulativeSkyMtx(epwfile,  skyDensity= skyDensity, workingDir = workingDir)
+cSky.gendaymtx()
 
 # assign outputs
-OUT = copy.deepcopy(slh.results)
-del(slh)
+OUT = cSky
