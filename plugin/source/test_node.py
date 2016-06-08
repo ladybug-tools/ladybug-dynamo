bbfab4e8c00100c12f4811f7bb24c0a666444d43
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-06-07 23:19:33 -0400

    Addressed https://github.com/ladybug-analysis-tools/ladybug-dynamo/issues/12#issuecomment-224125154

--- plugin/source/test_node.py
+++ plugin/source/test_node.py
@@ -2,16 +2,16 @@ import sys
 p = r"C:\Users\Administrator\AppData\Roaming\Dynamo\Dynamo Revit\1.0\packages\Ladybug\extra\ladybugdynamo"
 sys.path.append(p)
 #
-# from ladybug.analysisperiod import AnalysisPeriod
+
+from ladybug.analysisperiod import AnalysisPeriod
+
 # from ladybug.epw import EPW
-from ladybug.location import Location
-from ladybug.sunpath import LBSunpath
+# from ladybug.location import Location
+# from ladybug.sunpath import LBSunpath
 # from ladybug.dt import LBDateTime
 # from ladybug.datatype import SkyPatch, Temperature, LBData
+ap = AnalysisPeriod(stMonth=12, stDay=31,
+                    endMonth=1, endDay=1,
+                    timestep=2)
 
-loc = Location(city='B', latitude=42, longitude=-71)
-print repr(loc)
-sp = LBSunpath.fromLocation(loc)
-for h in range(4109, 4125):
-    sun = sp.calculateSunFromHOY(h)
-    print sun
+print ap.datetimes
